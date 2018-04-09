85583c4cf4f219a8f2473ccd174d401dd137a534
sateniktovmasyan <38066740+sateniktovmasyan@users.noreply.github.com>
2018-04-10 00:03:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import json
-def loadbreakdowngrades():
 
 
+def load_breakdown_grades():
     with open ('general_Setup.json') as Data1:
         course = json.load(Data1)
     breakdown = course['cSetup']['breakdown']
     return breakdown
 
-def loadgeneral_grades():
+
+def load_general_grades():
     with open ('general_grades.json') as Data2:
         general_grades = json.load(Data2)
         return general_grades
+
 
 def replacement(general_grades):
     for key in general_grades['totalgrades']:
@@ -20,13 +22,15 @@ def replacement(general_grades):
                 general_grades['totalgrades'][key] = input('So, what is your grade now???')
                 return general_grades
 
-def remember(updateddata):
-    print (json.dumps(updateddata))
+
+def remember(updated_data):
+    print (json.dumps(updated_data))
     file = open("general_grades.json")
-    file.write(json.dumps(updateddata))
+    file.write(json.dumps(updated_data))
     file.close()
 
-def printCurrentGrade(grades, grade_now):
+
+def print_Current_Grade(grades, grade_now):
     grade = 0
     for key in grade_now["totalgrades"]:
         if grade_now["totalgrades"][key] != -1:
@@ -34,25 +38,28 @@ def printCurrentGrade(grades, grade_now):
             grade = grade + total
     return (grade)
 
-def loadconversionmatrix():
+
+def load_conversion_matrix():
     with open ('general_Setup.json') as Data:
         general_Setup = json.load(Data)
         conversionmatrix = general_Setup['cSetup']['conversionmatrix']
     return conversionmatrix
 
-def printMessage(grade,conversionmatrix):
+
+def print_message(grade,conversionmatrix):
     for z in range(len(conversionmatrix)):
         if int(conversionmatrix[z]["max"]) >= int(grade) and int(conversionmatrix[z]["min"]) <= int(grade):
             print "now your current grade is" + str(grade) + " & mark for it is " + str(conversionmatrix[z]["mark"])
 
+
 def main():
-    breakdown = loadbreakdowngrades()
-    general_grades = loadgeneral_grades()
+    breakdown = load_breakdown_grades()
+    general_grades = load_general_grades()
     updateddata = replacement(general_grades)
     remember(updateddata)
     grade = printCurrentGrade(breakdown, updateddata)
     conversionmatrix = loadconversionmatrix()
-    printMessage(grade,conversionmatrix)
+    print_message(grade,conversionmatrix)
 
 main()
 
