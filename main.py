736b36a4854d448713fc29a59b1d9bdb0d69c593
sateniktovmasyan <38066740+sateniktovmasyan@users.noreply.github.com>
2018-04-10 14:36:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import json
 
 
 def load_breakdown_grades():
-    with open ('general_Setup.json') as Data1:
-        course = json.load(Data1)
+    with open('general_Setup.json') as data_file:
+        course = json.load(data_file)
     breakdown = course['cSetup']['breakdown']
     return breakdown
 
 
 def load_general_grades():
-    with open ('general_grades.json') as Data2:
-        general_grades = json.load(Data2)
-        return general_grades
+    with open('general_grades.json') as data_file:
+        general_grades = json.load(data_file)
+    return general_grades
 
 
 def replacement(general_grades):
     for key in general_grades['totalgrades']:
-        print"From now, your grade for " + key + 'has a value '+ str(general_grades['totalgrades'][key])
+        print"From now, your grade for " + key + 'has a value ' + str(general_grades['totalgrades'][key])
         x = raw_input('if change type yes, otherwise no')
-    if x == str('yes'):
-                general_grades['totalgrades'][key] = input('So, what is your grade now???')
-                return general_grades
+        if x == 'yes':
+            new_grade = input('So, what is your grade now???')
+            if new_grade <= 100 and new_grade >= 0:
+                general_grades['totalgrades'][key] = new_grade
+            else:
+                general_grades['totalgrades'][key] = input('Please input a grade between 0 and 100')
+    return general_grades
 
 
 def remember(updated_data):
@@ -31,17 +35,17 @@ def remember(updated_data):
 
 
 def print_Current_Grade(grades, grade_now):
-    grade = 0
+    grade = 0.0
     for key in grade_now["totalgrades"]:
         if grade_now["totalgrades"][key] != -1:
-            total = int(grade_now["totalgrades"][key]) * grades[key] / 100
+            total = float(grade_now["totalgrades"][key]) * grades[key] / 100
             grade = grade + total
-    return (grade)
+    return grade
 
 
 def load_conversion_matrix():
-    with open ('general_Setup.json') as Data:
-        general_Setup = json.load(Data)
+    with open('general_Setup.json') as data_file:
+        general_Setup = json.load(data_file)
         conversionmatrix = general_Setup['cSetup']['conversionmatrix']
     return conversionmatrix
 
@@ -60,6 +64,6 @@ def main():
     grade = printCurrentGrade(breakdown, updateddata)
     conversionmatrix = loadconversionmatrix()
     print_message(grade,conversionmatrix)
-
+    
 main()
 
